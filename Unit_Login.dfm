2cea61e6ca7fe5a3b439283656514e1e5db956a0
Andrei Ricardo Gomes Marques <andrei.ric@hotmail.com>
2025-12-12 13:17:34 -0400

    feat/Desenvolvido Login

---
+++ Unit_Login.dfm
@@ -0,0 +1,109 @@
+object FormLogin: TFormLogin
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu]
+  Caption = 'Form Login'
+  ClientHeight = 419
+  ClientWidth = 301
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Position = poScreenCenter
+  OnShow = FormShow
+  TextHeight = 15
+  object Image_BackGround: TImage
+    Left = 0
+    Top = 0
+    Width = 301
+    Height = 419
+    Align = alClient
+    Stretch = True
+    ExplicitLeft = 194
+    ExplicitTop = 12
+    ExplicitWidth = 105
+    ExplicitHeight = 105
+  end
+  object Panel1: TPanel
+    Left = 29
+    Top = 23
+    Width = 239
+    Height = 354
+    Color = clWhite
+    ParentBackground = False
+    TabOrder = 0
+    object Image_Logo: TImage
+      Left = 87
+      Top = 13
+      Width = 73
+      Height = 73
+      Proportional = True
+      Stretch = True
+    end
+    object Label_Login: TLabel
+      Left = 95
+      Top = 115
+      Width = 51
+      Height = 30
+      Alignment = taCenter
+      Caption = 'Login'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -21
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object LblDontHaveAccount: TLabel
+      Left = 18
+      Top = 312
+      Width = 121
+      Height = 15
+      Caption = 'Dont have an account?'
+    end
+    object LblSignUp: TLabel
+      Left = 174
+      Top = 312
+      Width = 41
+      Height = 15
+      Caption = 'Sign Up'
+      OnClick = LblSignUpClick
+    end
+    object Edit_UserName: TEdit
+      Left = 18
+      Top = 167
+      Width = 197
+      Height = 23
+      TabOrder = 0
+      TextHint = 'Username'
+    end
+    object Edit_Password: TEdit
+      Left = 14
+      Top = 215
+      Width = 160
+      Height = 23
+      PasswordChar = '*'
+      TabOrder = 1
+      TextHint = 'Password'
+    end
+    object Button_Login: TButton
+      Left = 79
+      Top = 265
+      Width = 75
+      Height = 25
+      Caption = 'Login'
+      TabOrder = 2
+      OnClick = Button_LoginClick
+    end
+    object Button_ShowPass: TButton
+      Left = 180
+      Top = 214
+      Width = 35
+      Height = 25
+      TabOrder = 3
+      OnClick = Button_ShowPassClick
+    end
+  end
+end
---
+++ Unit_Login.pas
@@ -0,0 +1,212 @@
+unit Unit_Login;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics, Vcl.Controls,
+  Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Menus, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
+  D2Bridge.Forms; // Declare D2Bridge.Forms always in the last unit
+
+type
+  TFormLogin = class(TD2BridgeForm)
+    Panel1: TPanel;
+    Image_Logo: TImage;
+    Label_Login: TLabel;
+    Edit_UserName: TEdit;
+    Edit_Password: TEdit;
+    Button_Login: TButton;
+    Image_BackGround: TImage;
+    Button_ShowPass: TButton;
+    LblDontHaveAccount: TLabel;
+    LblSignUp: TLabel;
+    procedure Button_LoginClick(Sender: TObject);
+    procedure Button_ShowPassClick(Sender: TObject);
+    procedure LblSignUpClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+
+  protected
+    procedure ExportD2Bridge; override;
+    procedure InitControlsD2Bridge(const PrismControl: TPrismControl); override;
+    procedure RenderD2Bridge(const PrismControl: TPrismControl; var HTMLControl: string); override;
+  end;
+
+function FormLogin: TFormLogin;
+
+implementation
+
+uses
+  D2RestaurantWebApp, Unit_Dashboard, Unit_MailConfirmation, Unit_DM, Useful;
+
+const
+  POPUP_MAIL_CONFIRMATION = 'PopupMailConfirmation';
+
+function FormLogin: TFormLogin;
+begin
+  Result := TFormLogin(TFormLogin.GetInstance);
+end;
+
+{$R *.dfm}
+{ TForm_Login }
+
+procedure TFormLogin.Button_LoginClick(Sender: TObject);
+begin
+  if not D2Bridge.API.Mail.IsValiAddress(Edit_UserName.Text) then
+  begin
+    D2Bridge.Validation(Edit_UserName, False);
+    D2Bridge.Validation(Edit_Password, False, 'Email invalid');
+    Exit;
+  end;
+
+  if Length(Edit_Password.Text) < 6 then
+  begin
+    D2Bridge.Validation(Edit_UserName, False);
+    D2Bridge.Validation(Edit_Password, False, 'Invalid username or password');
+    Exit;
+  end;
+
+  if DM = nil then
+    TDM.CreateInstance;
+
+  DM.QryUser.Close;
+  DM.QryUser.MacroByName('filter').AsRaw      := 'WHERE email = :email AND password = :password AND status = ''Active''';
+  DM.QryUser.ParamByName('email').AsString    := Edit_UserName.Text;
+  DM.QryUser.ParamByName('password').AsString := EncryptPassword(Edit_Password.Text);
+  DM.QryUser.Open;
+
+  if DM.QryUser.IsEmpty then
+  begin
+    D2Bridge.Validation(Edit_UserName, False);
+    D2Bridge.Validation(Edit_Password, False, 'Invalid username or password');
+    Exit;
+  end;
+
+  D2Restaurant.IdUser      := DM.QryUser.FieldByName('id').AsInteger;
+  D2Restaurant.IdAccount   := DM.QryUser.FieldByName('id_account').AsInteger;
+  D2Restaurant.UserName    := DM.QryUser.FieldByName('name').AsString;
+  D2Restaurant.UserAdmin   := SameText(DM.QryUser.FieldByName('admin').AsString, 'Yes');
+  D2Restaurant.UserAccount := SameText(DM.QryUser.FieldByName('account_user').AsString, 'Yes');
+  D2Restaurant.UserEmail   := DM.QryUser.FieldByName('email').AsString;
+
+  if FormDashboard = nil then
+    TFormDashboard.CreateInstance;
+  FormDashboard.Show;
+end;
+
+procedure TFormLogin.Button_ShowPassClick(Sender: TObject);
+begin
+  if Edit_Password.PasswordChar = '*' then
+  begin
+    Edit_Password.PasswordChar := #0;
+
+    if IsD2BridgeContext then
+      D2Bridge.PrismControlFromVCLObj(Edit_Password).AsEdit.DataType := TPrismFieldType.PrismFieldTypeString;
+  end
+  else begin
+    Edit_Password.PasswordChar := '*';
+
+    if IsD2BridgeContext then
+      D2Bridge.PrismControlFromVCLObj(Edit_Password).AsEdit.DataType := TPrismFieldType.PrismFieldTypePassword;
+  end;
+end;
+
+procedure TFormLogin.ExportD2Bridge;
+begin
+  inherited;
+  Title    := 'My D2Bridge Web Application';
+  SubTitle := 'My WebApp';
+
+  // Background color
+  D2Bridge.HTML.Render.BodyStyle := 'background-color: #f0f0f0;';
+
+  // TemplateClassForm:= TD2BridgeFormTemplate;
+  D2Bridge.FrameworkExportType.TemplateMasterHTMLFile := '';
+  D2Bridge.FrameworkExportType.TemplatePageHTMLFile   := '';
+
+  if FormMailConfirmation = nil then
+    TFormMailConfirmation.CreateInstance;
+  D2Bridge.AddNested(FormMailConfirmation);
+
+  // Export yours Controls
+  with D2Bridge.Items.add do
+  begin
+    // Image Backgroup Full Size *Use also ImageFromURL...
+//    ImageFromTImage(Image_BackGround, CSSClass.Image.Image_BG20_FullSize);
+    ImageFromLocal('support\images\bg\bg.jpg', CSSClass.Image.Image_BG40_FullSize);
+
+    with Card do
+    begin
+      CSSClasses := CSSClass.Card.Card_Center + ' ' + CSSClass.Space.spacemode;
+
+//      ImageICOFromTImage(Image_Logo, CSSClass.Col.ColSize4);
+      ImageICOFromLocal('support\images\logo\logod2restaurant.png', CSSClass.Col.colsize4);
+
+      with BodyItems.add do
+      begin
+        with Row.Items.add do
+          Col.add.VCLObj(Label_Login);
+
+        with Row.Items.add do
+          Col.add.VCLObj(Edit_UserName, 'ValidationLogin', true);
+
+        with Row.Items.add do
+          with Col.Items.add do // Example Edit + Button same row and col
+          begin
+            VCLObj(Edit_Password, 'ValidationLogin', true);
+            VCLObj(Button_ShowPass, CSSClass.Button.view);
+          end;
+
+        with Row.Items.add do
+          Col.add.VCLObj(Button_Login, 'ValidationLogin', false, CSSClass.Col.colsize12);
+
+        with Row(CSSClass.Space.SpaceMin_top_bottom).Items.Add do
+        begin
+          with Col(CSSClass.Col.Align.center).Items.Add do
+          begin
+            VCLObj(LblDontHaveAccount);
+            Link(LblSignUp);
+          end;
+        end;
+      end;
+    end;
+
+    with Popup(POPUP_MAIL_CONFIRMATION, '', False, CSSClass.Popup.default).Items.Add do
+      Nested(FormMailConfirmation);
+  end;
+end;
+
+procedure TFormLogin.FormShow(Sender: TObject);
+begin
+  if IsDebuggerPresent then
+  begin
+    Edit_UserName.Text := 'andrei@fontdata.com.br';
+    Edit_Password.Text := '123456789';
+  end;
+end;
+
+procedure TFormLogin.InitControlsD2Bridge(const PrismControl: TPrismControl);
+begin
+  inherited;
+
+end;
+
+procedure TFormLogin.LblSignUpClick(Sender: TObject);
+begin
+  ShowPopup(POPUP_MAIL_CONFIRMATION);
+end;
+
+procedure TFormLogin.RenderD2Bridge(const PrismControl: TPrismControl; var HTMLControl: string);
+begin
+  inherited;
+  // Intercept HTML
+  {
+    if PrismControl.VCLComponent = Edit1 then
+    begin
+    HTMLControl:= '</>';
+    end;
+  }
+end;
+
+end.
